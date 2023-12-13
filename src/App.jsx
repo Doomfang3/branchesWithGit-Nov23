2565b808abac90f9f9bda026f54d85c6201b5b4e
Doomfang3 <saubinmathieu@gmail.com>
2023-12-13 14:23:44 +0100

    Add nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
-import { Route, Routes } from "react-router-dom";
-// CAN YALL SEE THIS????
+import { Route, Routes, Link } from 'react-router-dom'
+
 function App() {
   return (
     <>
+      <nav>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+        </ul>
+      </nav>
       <Routes>
-        <Route path="/" element={<h1>My Home Page</h1>} />
+        <Route path='/' element={<h1>My Home Page</h1>} />
         {/* RAGNAR RULES ALL!!!! */}
-        <Route path="*" element={<h1>404 Not Found</h1>} />
+        <Route path='*' element={<h1>404 Not Found</h1>} />
       </Routes>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
