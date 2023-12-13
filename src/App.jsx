b251751a03e87086247ae1a829e366224647a8f1
Doomfang3 <saubinmathieu@gmail.com>
2023-12-13 14:18:04 +0100

    Add a fragment

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<h1>My Home Page</h1>} />
+    <>
+      <Routes>
+        <Route path='/' element={<h1>My Home Page</h1>} />
 
-      <Route path='*' element={<h1>404 Not Found</h1>} />
-    </Routes>
+        <Route path='*' element={<h1>404 Not Found</h1>} />
+      </Routes>
+    </>
   )
 }
 
