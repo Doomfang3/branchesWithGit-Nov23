4718c447dcdb03de6917f0fbd04441e5a6ec8eef
Joshua George <horheyinc8@gmail.com>
2023-12-13 14:23:33 +0100

    ragnars test commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { Route, Routes } from 'react-router-dom'
-
+import { Route, Routes } from "react-router-dom";
+// CAN YALL SEE THIS????
 function App() {
   return (
     <>
       <Routes>
-        <Route path='/' element={<h1>My Home Page</h1>} />
-
-        <Route path='*' element={<h1>404 Not Found</h1>} />
+        <Route path="/" element={<h1>My Home Page</h1>} />
+        {/* RAGNAR RULES ALL!!!! */}
+        <Route path="*" element={<h1>404 Not Found</h1>} />
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
